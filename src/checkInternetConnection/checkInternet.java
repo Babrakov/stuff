862f03eb1975a182a27eabb4e5607350c3dc715f
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-06-07 10:06:52 +0300

    Added check Internet snippet.

---
+++ src/checkInternetConnection/checkInternet.java
@@ -0,0 +1,17 @@
+package checkInternetConnection;
+
+import java.io.IOException;
+
+public class checkInternet {
+
+    public static void main(String[] args) throws Exception {
+        Process process = java.lang.Runtime.getRuntime().exec("ping ya.ru");
+        int x = process.waitFor();
+        if (x==0) {
+            System.out.println("Connection successful, "+ "Output was " + x);
+        } else {
+            System.out.println("Internet not connected, "+ "Output was " + x);
+        }
+    }
+
+}
